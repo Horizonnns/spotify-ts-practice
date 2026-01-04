09ddc6c091c84393ff614db1c419be6d3a00cd13
horizonnns <shadowcrewhack@gmail.com>
2026-01-04 23:34:07 +0500

    practice with interfaces and types

--- src/types/music.types.ts
+++ src/types/music.types.ts
@@ -6,14 +6,38 @@ type TImageUrl = string
 // type YArtist = null | string
 // type TAlbom = undefined | string
 
-interface IArtist {
+export type TStatus = 'idle' | 'loading' | 'successed' | 'failed'
+
+type TBase = {
 	id: TID
+	createdAt: string
+	updatedAt: string
+}
+
+export type TArtist = {
+	name: string
+	image: TImageUrl
+	isVerivied: boolean
+} & TBase
+
+interface IBase {
+	id: TID
+	createdAt: string
+	updatedAt: string
+}
+
+interface IArtist extends IBase {
 	name: string
 	image: TImageUrl
 	isVerivied: boolean
 }
 
-interface ITrack {
-	id: TID
+export interface ITrack extends IBase {
 	artist: IArtist
+	title: string
+	duration: number
+	isPublished: string
 }
+
+//  Interface -> используется для описание объекта, сущности (юзера, книги и т.д.), ответы от API
+// Type -> используется для описание типов, кот. не являются объектами (примитивы, объединения, пересечения и т.д.)
